224a939432f2056887cae2c595fd8129da68f4a6
Jose Chavez <darkaw_remse@hotmail.com>
2016-07-29 10:08:59 -0500

    Initial Commit

---
+++ show/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  show
+//
+//  Created by Jose Chavez on 29/07/16.
+//  Copyright © 2016 Jose Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
